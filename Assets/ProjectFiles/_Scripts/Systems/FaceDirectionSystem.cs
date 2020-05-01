5cce3d4a5730a3df6683cb7db2bdbb89c003e34a
KLEEEEEER <gonzalx_747@mail.ru>
2020-05-01 17:10:42 +0900

    Added player rotation basics on mouse pointer position on a screen and not on the floor

--- Assets/ProjectFiles/_Scripts/Systems/FaceDirectionSystem.cs
+++ Assets/ProjectFiles/_Scripts/Systems/FaceDirectionSystem.cs
@@ -8,20 +8,20 @@ using Unity.Physics.Systems;
 
 public class FaceDirectionSystem : SystemBase
 {
-    private BuildPhysicsWorld _buildPhysicsWorldSystem;
+    //private BuildPhysicsWorld _buildPhysicsWorldSystem;
     //private EndFramePhysicsSystem _endFramePhysicsSystem;
-    public Vector3 mouseHitPosition;
-    Unity.Physics.RaycastHit hit;
+    //public Vector3 mouseHitPosition;
+    //Unity.Physics.RaycastHit hit;
 
     protected override void OnStartRunning()
     {
-        _buildPhysicsWorldSystem = World.GetExistingSystem<BuildPhysicsWorld>();
+        //_buildPhysicsWorldSystem = World.GetExistingSystem<BuildPhysicsWorld>();
         //_endFramePhysicsSystem = World.GetOrCreateSystem<EndFramePhysicsSystem>();
     }
     protected override void OnUpdate()
     {
-        //if (mainCamera == null) return;
-        var screenRay = MonoBehaviourECSBridge.Instance.mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        /*var screenRay = MonoBehaviourECSBridge.Instance.mainCamera.ScreenPointToRay(Input.mousePosition);
 
         RaycastInput raycastInput = new RaycastInput
         {
@@ -33,18 +33,28 @@ public class FaceDirectionSystem : SystemBase
                 CollidesWith = ~0u,
                 GroupIndex = 0
             }
-        };
+        };*/
+        Vector3 mousePosition = Input.mousePosition;
 
         Entities.ForEach((ref Rotation rot, in Translation pos, in MovementData moveData) => //  in CameraBaseInputData cameraBaseInputData
         {
-            if (_buildPhysicsWorldSystem.PhysicsWorld.CollisionWorld.CastRay(raycastInput, out hit))
-            {
-                var newPosition = hit.Position - pos.Value;
-                newPosition.y = 0;
+            Vector3 entityCameraPosition = MonoBehaviourECSBridge.Instance.mainCamera.WorldToScreenPoint(pos.Value);
+            //var newPosition = mousePosition - entityCameraPosition;
+            //newPosition.y = 0;
 
-                quaternion targetRotation = quaternion.LookRotation(newPosition, math.up());
-                rot.Value = math.slerp(rot.Value, targetRotation, moveData.TurnSpeed);
-            }
+            //quaternion targetRotation = quaternion.LookRotation(newPosition.normalized, math.up());
+
+            //mousePosition.z = 5.23f;
+            mousePosition.x = mousePosition.x - entityCameraPosition.x;
+            mousePosition.y = mousePosition.y - entityCameraPosition.y;
+            float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+            angle -= 90f;
+
+            quaternion targetRotation = Quaternion.Euler(new Vector3(0, -angle, 0));
+
+
+           // Debug.Log($"newPosition.normalized = {newPosition.normalized}");
+            rot.Value = math.slerp(rot.Value, targetRotation, moveData.TurnSpeed);
         }).WithoutBurst().Run();
     }
 }
